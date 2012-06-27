7c99865e714610b15ed0c8a1397cc73dc6b81fdc
Kevin Qiu <kevin@freshbooks.com>
2012-06-27 01:05:14 -0400

    Added code to run pylint

--- aniketos/checker/pylint.py
+++ aniketos/checker/pylint.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 import shutil
 import sys
 import os
+import json
+from collections import defaultdict
 from pylint.reporters import BaseReporter
 from pylint.interfaces import IReporter
 from pylint.lint import Run
@@ -33,10 +35,11 @@ class JsonReporter(BaseReporter):
 
 class PylintChecker(object):
 
-    def __init__(self, git, staging_dir, previous_result_file):
+    def __init__(self, git, staging_dir, previous_result_file, rcfile=None):
         self.git = git
         self.staging_dir = staging_dir
         self.previous_result_file = previous_result_file
+        self.rcfile = rcfile
 
     def _nuke_dir_if_necessary(self, dir_):
         if os.path.exists(dir_):
@@ -55,12 +58,35 @@ class PylintChecker(object):
         self._nuke_dir_if_necessary(self.staging_dir)
         self._create_dir(self.staging_dir)
         # for each changed files, check out a local copy
+
+        abs_paths = []
         for file_, details in changed_file_details:
             abs_path = os.path.join(self.staging_dir, file_)
             self._create_dir(os.path.dirname(abs_path))
             with open(abs_path, 'w') as f:
                 f.write(self.git.get_blob(details['hash']))
+            abs_paths.append(abs_path)
 
-        print changed_file_details
+        result = self._run_pylint(abs_paths)
+        print result
+
+    def _run_pylint(self, abs_paths):
+        # TODO: a better idiom for 'throwing away output'?
+        import cStringIO
+        output = cStringIO.StringIO()
+        reporter = JsonReporter(self.staging_dir, output=output)
+        del output
+
+        if self.rcfile:
+            args = ['--rcfile %s' % self.rcfile]
+        else:
+            args = []
+        args.extend(list(abs_paths))
+
+        Run(args,
+            reporter=reporter,
+            exit=False)
+
+        return reporter.messages
 
 # vim: set fdm=marker foldlevel=1:
